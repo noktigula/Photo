e8fe088c465e816ecb6ee0fa9482f9c7acea3103
noktigula@gmail.com <noktigula@gmail.com>
2012-11-07 11:58:45 +0400

    okay

--- protected/views/site/services.php
+++ protected/views/site/services.php
@@ -1,1 +1,48 @@
-<h1>services</h1>
+<!--<h1>services</h1>-->
+<div class='service'>
+    <table cellpadding="0px" cellspacing="0px" border='0px'>
+        <tr>
+            <td>
+                <a href="<?php echo Yii::app()->getUrlManager()->createUrl('/portfolio/weddings')?>">
+                <img src='images/services/weddings.jpg' width="225px" height="150px">
+                </a>
+            </td>
+            <td valign="top">
+                текст текст текст текст текст
+            </td>
+        </tr>
+    </table>
+</div>
+
+<br />
+
+<div class='service'>
+    <table cellpadding="0px" cellspacing="0px" border='0px'>
+        <tr>
+            <td><a href="<?php echo Yii::app()->getUrlManager()->createUrl('/portfolio/kids')?>">
+                <img src='images/services/kids.jpg' width="225px" height="150px">
+            </a>
+            </td>
+            <td valign="top">
+                текст текст текст текст текст
+            </td>
+        </tr>
+    </table>
+</div>
+
+<br />
+
+<div class='service'>
+    <table cellpadding="0px" cellspacing="0px" border='0px'>
+        <tr>
+            <td>
+                <a href="<?php echo Yii::app()->getUrlManager()->createUrl('/portfolio/portraits') ?>">
+                    <img src='images/services/portraits.jpg' width="225px" height="150px">
+                </a>
+            </td>
+            <td valign="top">
+                текст текст текст текст текст
+            </td>
+        </tr>
+    </table>
+</div>
